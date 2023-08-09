f7cd4b0abf9d08905046716545a34f67011b973d
Sameesh Sathyan <sameesh241@gmail.com>
2023-08-09 22:28:57 +0530

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,6 +30,8 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "Apple"
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
+# pip install -r requirements.txt
+
 import snowflake
 import snowflake.connector
 
