12e52080472fae2d7d818c28eb54407d73d976d3
Sameesh Sathyan <sameesh241@gmail.com>
2023-08-09 23:20:22 +0530

    correcntion in insert function call

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,7 +60,7 @@ def insert_row_sf(new_fruit):
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
 if streamlit.button('Add a Fruit to the List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-    back_from_function = insert_row_sf(fruit_choice)
+    back_from_function = insert_row_sf(add_my_fruit)
     streamlit.text(back_from_function) 
 
 
