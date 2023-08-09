49ba9ebbe6b0e97957b80f7c8b86c3f341d95873
Sameesh Sathyan <sameesh241@gmail.com>
2023-08-09 22:48:14 +0530

    add input text

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered', fruit_choice)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "Apple")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #streamlit.text(fruityvice_response.json())
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
@@ -42,3 +42,4 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
+fruit_choice = streamlit.text_input('What fruit would you like to add?','Jackfruit')
